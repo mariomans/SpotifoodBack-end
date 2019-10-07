f40a3924b3d93539a543af224d54ef84db617d09
Unknown <frong_98_@hotmail.com>
2019-10-07 20:44:48 +0700

    Add router of like and unlike

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost, photo, advertisement, singlePost } = require('../controllers/post');
+const { getPosts, createPost, postsByUser, postById, isPoster, deletePost, updatePost, photo, advertisement, singlePost, like, unlike } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 const { createPostValidator } = require('../validator')
 
 const router = express.Router();
 router.get('/posts', getPosts);
+
+//like unlike
+router.put('/post/like', requireSignin, like)
+router.put('/post/unlike', requireSignin, unlike)
+
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
 router.get('/posts/by/:userId', requireSignin, postsByUser);
 router.get('/post/:postId', singlePost);
