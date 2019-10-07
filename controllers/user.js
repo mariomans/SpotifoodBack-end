c82f83ccbcc8ebfa781f85edb7ce0d7896c43adb
Unknown <frong_98_@hotmail.com>
2019-10-07 13:49:41 +0700

    Add auto generate personalization when user signup

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,7 @@ exports.allUsers = (req, res) => {
             });
         }
         res.json(users);
-    }).select("name email updated created role");
+    }).select("name email updated created role personalization");
 };
 
 exports.getUser = (req, res) => {
