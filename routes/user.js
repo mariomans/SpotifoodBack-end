c3eddcef6711be21ecf132de9a020d94b21472ee
Unknown <frong_98_@hotmail.com>
2019-10-22 23:36:16 +0700

    Update admin role in database

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { userById, allUsers, getUser, updateUser, deleteUser, userPhoto , addFollowing , addFollower, removeFollowing , removeFollower} = require('../controllers/user');
+const { userById, allUsers, getUser, updateUser, deleteUser, userPhoto, addFollowing, addFollower, removeFollowing, removeFollower, hasAuthorization } = require('../controllers/user');
 const { requireSignin } = require('../controllers/auth');
 
 const router = express.Router();
 
-router.put('/user/follow', requireSignin, addFollowing , addFollower);
-router.put('/user/unfollow', requireSignin, removeFollowing , removeFollower);
+router.put('/user/follow', requireSignin, addFollowing, addFollower);
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower);
 
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
-router.put('/user/:userId', requireSignin, updateUser);
-router.delete('/user/:userId', requireSignin, deleteUser);
+router.put('/user/:userId', requireSignin, hasAuthorization, updateUser);
+router.delete('/user/:userId', requireSignin, hasAuthorization, deleteUser);
 // photo
 router.get('/user/photo/:userId', userPhoto);
 
